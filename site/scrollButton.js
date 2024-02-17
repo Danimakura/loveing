5bf1e7e9590f4ebc48b4b1b9c3e2e05a72750023
Danimakura <160320080+Danimakura@users.noreply.github.com>
2024-02-17 14:21:36 +0300

    Add files via upload

---
+++ site/scrollButton.js
@@ -0,0 +1,9 @@
+function changeKitty(mood) {
+    var kittyImg = document.getElementById('kittyImg');
+    if (mood === 'happy') {
+      kittyImg.src = 'https://i.pinimg.com/originals/d0/d0/32/d0d032fd6e907316d33ebdd894752518.gif';
+    } else if (mood === 'sad') {
+      kittyImg.src = 'https://i.pinimg.com/originals/d0/d0/32/d0d032fd6e907316d33ebdd894752518.gif';
+    }
+  }
+  
